cda0ee9cb247a751798ca2bf9d42df7de79bd482
chitragarj <40055172+chitragarj@users.noreply.github.com>
2019-12-31 16:04:48 +0000

    initial

---
+++ src/main/java/com/ginnie/Ginnie/GinnieApplication.java
@@ -0,0 +1,13 @@
+package com.ginnie.Ginnie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GinnieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GinnieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ginnie/Ginnie/GinnieApplicationTests.java
@@ -0,0 +1,10 @@
+package com.ginnie.Ginnie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GinnieApplicationTests {
+
+
+}
